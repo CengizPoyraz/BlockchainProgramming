5ca3a492f002862ceca09bea45931ae6587e25b7
Cengiz Poyraz <cengizp@hotmail.com>
2025-01-09 01:19:48 +0300

    docker updated

--- docker/start.bat
+++ docker/start.bat
@@ -1,1 +1,1 @@
-docker run -it --mount type=bind,src=//c/Users/cengi/DockerMount,target=/home/root/host --gpus all -p 8551 cpoyraz/blockchain:v1.5 bash
+docker run -it --mount type=bind,src=//c/Users/cengi/DockerMount,target=/home/root/host --gpus all -p 8551 -p 3000 cpoyraz/blockchain:v1.5 bash
